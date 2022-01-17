0fa3bd240e74cb5dd386b3a12d3f68ba34d1edeb
Filipp Pashkevich <fpashkevich@pstlabs.by>
2022-01-17 13:10:06 +0300

    modifyed employee form controls

--- src/app/pages/profile/pages/form/components/professional/roles/employee/employee.component.ts
+++ src/app/pages/profile/pages/form/components/professional/roles/employee/employee.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, OnDestroy, Input } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-import { Dictionaries } from "@app/store/dictionaries";
-import { ExperienceForm } from "./experiences/experiences.component";
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Dictionaries} from "@app/store/dictionaries";
+import {ExperienceForm} from "./experiences/experiences.component";
+
+import { ControlEntities, mapControls } from "@app/shared/utils/form";
 
 export interface EmployeeForm {
   specialization: string;
@@ -25,15 +27,19 @@ export class EmployeeComponent implements OnInit, OnDestroy {
   @Input() dictionaries: Dictionaries;
 
   form: FormGroup;
+  controls: ControlEntities;
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.initForm();
+    this.initControls();
 
     if (this.value) {
       this.form.patchValue(this.value);
     }
+
+    mapControls(this.controls);
 
     this.parent.addControl(this.name, this.form);
   }
@@ -52,15 +58,53 @@ export class EmployeeComponent implements OnInit, OnDestroy {
         updateOn: 'change',
         validators: [Validators.required]
       }],
-      qualification: [null, {
+      qualification: [{ value: null, disabled: true }, {
         updateOn: 'change',
         validators: [Validators.required]
       }],
-      skills: [null, {
+      skills: [{ value: null, disabled: true }, {
         updateOn: 'change',
         validators: [Validators.required]
       }]
     });
+  }
+
+  initControls(): void {
+    this.controls = {
+      specialization: {
+        items: this.dictionaries.specializations.controlItems,
+        changed: () => {
+          this.controls.qualification.map();
+          this.controls.skills.map();
+        }
+      },
+      qualification: {
+        items: this.dictionaries.qualifications.controlItems,
+        map: () => {
+          if (this.form.value.specialization) {
+            this.form.controls.qualification.enable();
+          } else {
+            this.form.controls.qualification.reset();
+            this.form.controls.qualification.disable();
+          }
+        }
+      },
+      skills: {
+        items: this.dictionaries.skills.controlItems,
+        map: () => {
+          if (this.form.value.specialization) {
+            this.form.controls.skills.enable();
+
+            this.controls.skills.items = [...this.dictionaries.skills.controlItems].map(
+              (item, index) => ({...item, label: `${item.label}  (${index + 1})`})
+            );
+          } else {
+            this.form.controls.skills.reset();
+            this.form.controls.skills.disable();
+          }
+        }
+      }
+    }
   }
 
 }
